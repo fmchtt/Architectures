cdb5996fd991b954bf017432f89b19517950114d
fmchtt <felipe@fmchtt.dev.br>
2023-10-19 11:22:07 -0300

    Diminuição de arquivo de testes e adição de teste de listagem de produtos

--- Architectures.Tests/ProductListTest.js
+++ Architectures.Tests/ProductListTest.js
@@ -2,17 +2,14 @@ import http from 'k6/http';
 import { check, fail } from 'k6';
 import { SharedArray } from 'k6/data';
 import { scenario } from 'k6/execution';
-import { FormData } from 'https://jslib.k6.io/formdata/0.0.2/index.js'
 
 const data = new SharedArray('users', () => {
   return JSON.parse(open('./data/user-faked.json'));
 })
 
-const xslx = open('./data/super-small-product-faked.xlsx', 'b');
-
 export const options = {
   scenarios: {
-    'uploadProdutos': {
+    'listProducts': {
       executor: 'shared-iterations',
       iterations: data.length,
       vus: 50,
@@ -25,9 +22,6 @@ export default function () {
   const urlProduto = 'http://localhost/produtos/';
 
   const user = data[scenario.iterationInTest];
-
-  const fd = new FormData();
-  fd.append('arquivo', http.file(xslx, 'product-faked.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'))
 
   const paramsLogin = {
     headers: {
@@ -44,14 +38,14 @@ export default function () {
 
   const paramsUploadProduto = {
     headers: {
-      'Content-Type': 'multipart/form-data;  boundary=' + fd.boundary,
       'Authorization': `Bearer ${loginRes.json().token}`
     },
   };
 
-  const productRes = http.post(urlProduto, fd.body(), paramsUploadProduto);
+  const productRes = http.get(urlProduto, paramsUploadProduto);
 
   check(productRes, {
-    'product creation is working': (r) => r.status === 200
-  }) || fail(`Erro ao efetuar uploads dos productos: ${user.nome} com a senha: ${user.senha}, resposta: ${productRes.body}`);
+    'product list is working': (r) => r.status === 200,
+    'product amount is correct': (r) => r.status === 200 && r.json().length === 1000,
+  }) || fail(`Erro ao efetuar listagem dos productos: ${user.nome} com a senha: ${user.senha}, resposta: ${productRes.body}`);
 }
